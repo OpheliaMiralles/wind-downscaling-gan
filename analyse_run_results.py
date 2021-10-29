c00cf5036c4156b8bd79b04214aac42e9514f0d8
OpheliaMiralles <opheliamiralles@gmail.com>
2021-10-29 15:26:52 +0200

    update result analysis scripts

--- analyse_run_results.py
+++ analyse_run_results.py
@@ -13,7 +13,7 @@ from data.data_generator import BatchGenerator, NaiveDecoder, LocalFileProvider,
 from data.data_generator import FlexibleNoiseGenerator
 from gan import train, metrics
 from gan.ganbase import GAN
-from gan.models import make_generator, make_discriminator
+from gan.models import make_generator, make_discriminator, make_generator_no_noise
 
 DATA_ROOT = Path('./data')
 PROCESSED_DATA_FOLDER = DATA_ROOT / 'img_prediction_files'
@@ -29,6 +29,7 @@ def plot_prediction(run_id, start_date, end_date, sequence_length=6,
                     data_provider: str = 'local',
                     saving_frequency=10,
                     nb_epochs=500,
+                    gen_only=False
                     ):
     TOPO_PREDICTORS = ['tpi_500', 'slope', 'aspect']
     HOMEMADE_PREDICTORS = ['e_plus', 'e_minus', 'w_speed', 'w_angle']
@@ -80,30 +81,6 @@ def plot_prediction(run_id, start_date, end_date, sequence_length=6,
     outputs = np.concatenate(outputs)
     INPUT_CHANNELS = len(ALL_INPUTS)
     OUT_CHANNELS = len(ALL_OUTPUTS)
-    # Creating GAN
-    generator = make_generator(image_size=img_size, in_channels=INPUT_CHANNELS,
-                               noise_channels=noise_channels, out_channels=OUT_CHANNELS,
-                               n_timesteps=sequence_length)
-    print(f"Generator: {generator.count_params():,} weights")
-    discriminator = make_discriminator(low_res_size=img_size, high_res_size=img_size, low_res_channels=INPUT_CHANNELS,
-                                       high_res_channels=OUT_CHANNELS, n_timesteps=sequence_length)
-    print(f"Discriminator: {discriminator.count_params():,} weights")
-    noise_shape = (batch_size, sequence_length, img_size, img_size, noise_channels)
-    gan = GAN(generator, discriminator, noise_generator=FlexibleNoiseGenerator(noise_shape, std=1))
-    print(f"Total: {gan.generator.count_params() + gan.discriminator.count_params():,} weights")
-    gan.compile(generator_optimizer=train.generator_optimizer(),
-                generator_metrics=[metrics.AngularCosineDistance(),
-                                   metrics.LogSpectralDistance(),
-                                   metrics.WeightedRMSEForExtremes(),
-                                   metrics.WindSpeedWeightedRMSE(),
-                                   metrics.SpatialKS()],
-                discriminator_optimizer=train.discriminator_optimizer(),
-                discriminator_loss=train.discriminator_loss,
-                metrics=[metrics.discriminator_score_fake(), metrics.discriminator_score_real()])
-    # Saving results
-    checkpoint_path_weights = Path(
-        './checkpoints/gan') / run_id / 'weights-{epoch:02d}.ckpt'
-    checkpoint_path_weights.parent.mkdir(exist_ok=True, parents=True)
 
     # Plotting some results
     def show(images, dims=1, legends=None):
@@ -116,17 +93,73 @@ def plot_prediction(run_id, start_date, end_date, sequence_length=6,
                 ax.axis('off')
         return fig
 
-    for epoch in np.arange(saving_frequency, nb_epochs, saving_frequency):
-        gan.load_weights(str(checkpoint_path_weights).format(epoch=epoch))
-        noise = FlexibleNoiseGenerator(noise_shape)()
-        for i in range(0, batch_size * 6, batch_size):
-            results = gan.generator.predict([inputs[i:i + batch_size], noise])
-            j = 0
-            for inp, out, res in zip(inputs[i:i + batch_size], outputs[i:i + batch_size],
-                                     results):
-                plot_path = Path(
-                    './plots/gan_pred') / run_id / str(epoch) / f'inp_{i}_batch_{j}.png'
-                plot_path.parent.mkdir(exist_ok=True, parents=True)
-                fig = show([inp, out, res])
-                fig.savefig(plot_path)
-                j += 1
+    if not gen_only:
+        # Creating GAN
+        generator = make_generator(image_size=img_size, in_channels=INPUT_CHANNELS,
+                                   noise_channels=noise_channels, out_channels=OUT_CHANNELS,
+                                   n_timesteps=sequence_length)
+        print(f"Generator: {generator.count_params():,} weights")
+        discriminator = make_discriminator(low_res_size=img_size, high_res_size=img_size,
+                                           low_res_channels=INPUT_CHANNELS,
+                                           high_res_channels=OUT_CHANNELS, n_timesteps=sequence_length)
+        print(f"Discriminator: {discriminator.count_params():,} weights")
+        noise_shape = (batch_size, sequence_length, img_size, img_size, noise_channels)
+        gan = GAN(generator, discriminator, noise_generator=FlexibleNoiseGenerator(noise_shape, std=1))
+        print(f"Total: {gan.generator.count_params() + gan.discriminator.count_params():,} weights")
+        gan.compile(generator_optimizer=train.generator_optimizer(),
+                    generator_metrics=[metrics.AngularCosineDistance(),
+                                       metrics.LogSpectralDistance(),
+                                       metrics.WeightedRMSEForExtremes(),
+                                       metrics.WindSpeedWeightedRMSE(),
+                                       metrics.SpatialKS()],
+                    discriminator_optimizer=train.discriminator_optimizer(),
+                    discriminator_loss=train.discriminator_loss,
+                    metrics=[metrics.discriminator_score_fake(), metrics.discriminator_score_real()])
+        # Saving results
+        checkpoint_path_weights = Path(
+            './checkpoints/gan') / run_id / 'weights-{epoch:02d}.ckpt'
+        checkpoint_path_weights.parent.mkdir(exist_ok=True, parents=True)
+
+        for epoch in np.arange(saving_frequency, nb_epochs, saving_frequency):
+            gan.load_weights(str(checkpoint_path_weights).format(epoch=epoch))
+            noise = FlexibleNoiseGenerator(noise_shape)()
+            for i in range(0, batch_size * 6, batch_size):
+                results = gan.generator.predict([inputs[i:i + batch_size], noise])
+                j = 0
+                for inp, out, res in zip(inputs[i:i + batch_size], outputs[i:i + batch_size],
+                                         results):
+                    plot_path = Path(
+                        './plots/gan_pred') / run_id / str(epoch) / f'inp_{i}_batch_{j}.png'
+                    plot_path.parent.mkdir(exist_ok=True, parents=True)
+                    fig = show([inp, out, res])
+                    fig.savefig(plot_path)
+                    j += 1
+
+    else:
+        generator = make_generator_no_noise(image_size=img_size, in_channels=INPUT_CHANNELS,
+                                            out_channels=OUT_CHANNELS,
+                                            n_timesteps=sequence_length)
+        print(f"Generator: {generator.count_params():,} weights")
+        generator.compile(optimizer=train.generator_optimizer(),
+                          metrics=[metrics.AngularCosineDistance(),
+                                   metrics.LogSpectralDistance(),
+                                   metrics.WeightedRMSEForExtremes(),
+                                   metrics.WindSpeedWeightedRMSE(),
+                                   metrics.SpatialKS()],
+                          loss=train.generator_loss)
+        # Saving results
+        checkpoint_path_weights = Path('./checkpoints/generator') / run_id / 'weights-{epoch:02d}.ckpt'
+
+        for epoch in np.arange(saving_frequency, nb_epochs, saving_frequency):
+            generator.load_weights(str(checkpoint_path_weights).format(epoch=epoch))
+            for i in range(0, batch_size * 6, batch_size):
+                results = generator.predict(inputs[i:i + batch_size])
+                j = 0
+                for inp, out, res in zip(inputs[i:i + batch_size], outputs[i:i + batch_size],
+                                         results):
+                    plot_path = Path(
+                        './plots/gen_pred') / run_id / str(epoch) / f'inp_{i}_batch_{j}.png'
+                    plot_path.parent.mkdir(exist_ok=True, parents=True)
+                    fig = show([inp, out, res])
+                    fig.savefig(plot_path)
+                    j += 1
